2611e337284babe4e6d81a526d9972599696611a
kadavr666 <akhmetovtimur@gmail.com>
2020-12-02 00:15:02 +0400

    adding css

--- .eleventy.js
+++ .eleventy.js
@@ -2,3 +2,9 @@ module.exports = function (eleventyConfig ) {
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("admin");
 }
+const CleanCSS = require("clean-css");
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addFilter("cssmin", function(code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
+};
