ca5349bbfbeebfa957d5f1f38d27113f7294d478
kadavr666 <akhmetovtimur@gmail.com>
2020-12-02 17:44:31 +0400

    adding a footer

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,4 @@
 module.exports = function (eleventyConfig ) {
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("admin");
-}
-const CleanCSS = require("clean-css");
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addFilter("cssmin", function(code) {
-    return new CleanCSS({}).minify(code).styles;
-  });
 };
